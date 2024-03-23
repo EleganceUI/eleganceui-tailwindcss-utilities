14738f181af092447a63d59a250cae7ce8724495
Éverton Toffanetto <evertondgn@hotmail.com>
2024-03-22 22:19:24 -0300

    fix: enhance border color utility in Tailwind CSS
    The update introduces a way to flatten color entries in the theme, replacing the previous values with an object derived from the newly generated flatten colors. This allows seamless usage of variants or shades in tailwind's color configurations, increasing color manipulation possibilities and offering flexibility in styling options.

--- src/utilities/borderColor.ts
+++ src/utilities/borderColor.ts
@@ -1,6 +1,16 @@
 import plugin from 'tailwindcss/plugin'
 
 export const borderColor = plugin(({ matchUtilities, theme }) => {
+  const colorEntries = Object.entries(theme('colors') || {})
+  const flattenColors = colorEntries.flatMap(([key, value]) => {
+    if (typeof value === 'object') {
+      return Object.entries(value).map(([shade, colorValue]) => [
+        `${key}-${shade}`,
+        colorValue
+      ])
+    }
+    return [[key, value]]
+  })
   matchUtilities(
     {
       bc: value => ({
@@ -8,8 +18,8 @@ export const borderColor = plugin(({ matchUtilities, theme }) => {
       })
     },
     {
-      values: theme('colors'),
-      type: 'any'
+      values: Object.fromEntries(flattenColors),
+      type: 'color'
     }
   )
 })
